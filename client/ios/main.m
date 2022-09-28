d7da532b6347b013862aea512bc1fb9ea461e883
Andrey Filipenkov <decapitator@ukr.net>
2022-09-28 14:49:46 +0300

    ios9

--- client/ios/main.m
+++ client/ios/main.m
@@ -15,43 +15,43 @@
 
 #include <stdlib.h>
 
-static void startSDLManually(int argc, char * argv[])
-{
-	id<UIApplicationDelegate> appDelegate;
-	@autoreleasepool {
-		__auto_type sdlAppDelegateClass = NSClassFromString(@"SDLUIKitDelegate");
-		NSCAssert(sdlAppDelegateClass != nil, @"SDL AppDelegate class doesn't exist");
-		NSCAssert(class_conformsToProtocol(sdlAppDelegateClass, @protocol(UIApplicationDelegate)), @"SDL AppDelegate doesn't conform to UIApplicationDelegate");
-		appDelegate = [sdlAppDelegateClass new];
-	}
-	UIApplication.sharedApplication.delegate = appDelegate;
+//static void startSDLManually(int argc, char * argv[])
+//{
+//	id<UIApplicationDelegate> appDelegate;
+//	@autoreleasepool {
+//		__auto_type sdlAppDelegateClass = NSClassFromString(@"SDLUIKitDelegate");
+//		NSCAssert(sdlAppDelegateClass != nil, @"SDL AppDelegate class doesn't exist");
+//		NSCAssert(class_conformsToProtocol(sdlAppDelegateClass, @protocol(UIApplicationDelegate)), @"SDL AppDelegate doesn't conform to UIApplicationDelegate");
+//		appDelegate = [sdlAppDelegateClass new];
+//	}
+//	UIApplication.sharedApplication.delegate = appDelegate;
+//
+//	int result = startSDL(argc, argv, YES);
+//	exit(result);
+//}
 
-	int result = startSDL(argc, argv, YES);
-	exit(result);
-}
-
-int qt_main_wrapper(int argc, char * argv[]);
+//int qt_main_wrapper(int argc, char * argv[]);
 
 int client_main(int argc, char * argv[])
 {
-	NSInteger launchType;
-	__auto_type envVar = getenv("VCMI_LAUNCH_TYPE");
-	if (envVar)
-		launchType = envVar[0] == '0' ? 0 : 1;
-	else {
-		@autoreleasepool {
-			launchType = [NSUserDefaults.standardUserDefaults integerForKey:@"LaunchType"];
-		}
-	}
-	if (launchType == 1)
+//	NSInteger launchType;
+//	__auto_type envVar = getenv("VCMI_LAUNCH_TYPE");
+//	if (envVar)
+//		launchType = envVar[0] == '0' ? 0 : 1;
+//	else {
+//		@autoreleasepool {
+//			launchType = [NSUserDefaults.standardUserDefaults integerForKey:@"LaunchType"];
+//		}
+//	}
+//	if (launchType == 1)
 		return startSDL(argc, argv, NO);
 
-	@autoreleasepool {
-		id __block startGameObserver = [NSNotificationCenter.defaultCenter addObserverForName:@"StartGame" object:nil queue:nil usingBlock:^(NSNotification * _Nonnull note) {
-			[NSNotificationCenter.defaultCenter removeObserver:startGameObserver];
-			startGameObserver = nil;
-			startSDLManually(argc, argv);
-		}];
-		return qt_main_wrapper(argc, argv);
-	}
+//	@autoreleasepool {
+//		id __block startGameObserver = [NSNotificationCenter.defaultCenter addObserverForName:@"StartGame" object:nil queue:nil usingBlock:^(NSNotification * _Nonnull note) {
+//			[NSNotificationCenter.defaultCenter removeObserver:startGameObserver];
+//			startGameObserver = nil;
+//			startSDLManually(argc, argv);
+//		}];
+//		return qt_main_wrapper(argc, argv);
+//	}
 }
