0e06f91284a6908c421d92933dcad5615e450ce8
Andrey Filipenkov <decapitator@ukr.net>
2025-10-02 16:10:11 +0300

    wip

--- android/vcmi-app/src/main/java/eu/vcmi/vcmi/ActivityLauncher.java
+++ android/vcmi-app/src/main/java/eu/vcmi/vcmi/ActivityLauncher.java
@@ -21,7 +21,7 @@ import org.libsdl.app.SDL;
 /**
  * @author F
  */
-public class ActivityLauncher extends org.qtproject.qt5.android.bindings.QtActivity
+public class ActivityLauncher extends org.qtproject.qt.android.bindings.QtActivity
 {
     private static final int PICK_EXTERNAL_VCMI_DATA_TO_COPY = 1;
 
