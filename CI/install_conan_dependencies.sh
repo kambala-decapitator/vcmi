22f246c94ec513211cf9e64f495a24c85342740a
Andrey Filipenkov <decapitator@ukr.net>
2026-06-28 22:33:59 +0300

    build for Android 4.4

--- CI/install_conan_dependencies.sh
+++ CI/install_conan_dependencies.sh
@@ -2,7 +2,7 @@
 
 RELEASE_TAG="2026-03-11"
 FILENAME="$1.txz"
-DOWNLOAD_URL="https://github.com/vcmi/vcmi-dependencies/releases/download/$RELEASE_TAG/$FILENAME"
+DOWNLOAD_URL="https://github.com/vcmi/vcmi-dependencies/actions/runs/28319294726/artifacts/7933962392"
 
 downloadedFile="$RUNNER_TEMP/$FILENAME"
 curl -Lo "$downloadedFile" "$DOWNLOAD_URL"
