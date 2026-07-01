4dfa1670d242b2630722dfe51d094738cf84a923
Andrey Filipenkov <decapitator@ukr.net>
2026-07-01 16:15:56 +0300

    update

--- CI/install_conan_dependencies.sh
+++ CI/install_conan_dependencies.sh
@@ -4,7 +4,7 @@ RELEASE_TAG="2026-03-11"
 FILENAME="$1.txz"
 
 downloadedFile="dependencies-android-armeabi-v7a"
-gh run download 28319294726 \
+gh run download 28517689856 \
 	--repo vcmi/vcmi-dependencies \
 	--dir "$RUNNER_TEMP" \
 	--name "$downloadedFile"
